600bebb40208bd4bb1261a5816703ec784041c71
grillopress <grillopress@gmail.com>
2016-03-03 00:37:18 +0000

    add sass and compass to theme

--- config.rb
+++ config.rb
@@ -1,3 +1,6 @@
+require 'bootstrap-sass'
+require 'compass/import-once/activate'
+
 http_path = "/" #root level target path
 css_dir = "." #targets our default style.css file at the root level of our theme
 sass_dir = "sass" #targets our sass directory
