bc8ec4c2b98209fc0baa941172d4d5b23cb9564a
GrilloPress <grillopress@gmail.com>
2016-05-27 21:48:39 +0000

    add reason section acf fields

--- page-home.php
+++ page-home.php
@@ -128,14 +128,10 @@ get_header(); ?>
       </div>
     </div>
   </section>
-     
-     
-       <div class="row">
-      
-      <main id="main" class="col-md-6" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
+  
+  <div class="row">
+    <main id="main" class="col-md-6" role="main">
+      <?php while ( have_posts() ) : the_post(); ?>
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
           <div class="entry-content">
             <?php the_content(); ?>
@@ -160,133 +156,204 @@ get_header(); ?>
       </aside>
       
 	  </div><!-- #primary -->
-     
+  
+  <section class="front-page-featured-reasons">
 
-     <section class="front-page-featured-reasons">
-      <div class="row">
-        
-        
-        <div class="col-md-5 col-md-offset-2">
-          <div class="media">
-            <div class="media-left">
-              <a href="#">
-                <img src="http://placehold.it/50x50" alt="..." class="img-circle">
+    <div class="row">
+      <div class="col-md-5 col-md-offset-2">
+        <div class="media">
+          <div class="media-left">      
+            <?php if( get_field('reasons_link_one') ): ?>
+              <a href="<?php the_field('reasons_link_one'); ?>">
+                 <?php if( get_field('reasons_image_one') ): ?>
+                    <img src="<?php the_field('reasons_image_one'); ?>" class="img-circle" width="50" height="50">
+                  <?php endif; ?>
               </a>
-            </div>
-            <div class="media-body">
-              <h4 class="media-heading">Media heading</h4>
-              ...
-            </div>
+            <?php endif; ?>
           </div>
-        </div>
-        
-        <div class="col-md-5">
-          <div class="media">
-            <div class="media-left">
-              <a href="#">
-                <img src="http://placehold.it/50x50" alt="..." class="img-circle">
-              </a>
-            </div>
-            <div class="media-body">
-              <h4 class="media-heading">Media heading</h4>
-              ...
-            </div>
-          </div>
+          
+          <?php if( get_field('reasons_heading_one') ): ?>
+              <div class="media-body">
+                <h4 class="media-heading"><?php the_field('reasons_heading_one'); ?></h4>
+                <?php if( get_field('reasons_body_one') ): ?>
+                  <?php if( get_field('reasons_body_one') ): ?>
+                <?php endif; ?>
+              </div>        
+          <?php endif; ?>
         </div>
       </div>
-       
-       <div class="row">
-        <div class="col-md-5 col-md-offset-2">
-          <div class="media">
-            <div class="media-left">
-              <a href="#">
-                <img src="http://placehold.it/50x50" alt="..." class="img-circle">
-              </a>
-            </div>
-            <div class="media-body">
-              <h4 class="media-heading">Media heading</h4>
-              ...
-            </div>
-          </div>
-        </div>
         
-        <div class="col-md-5">
-          <div class="media">
-            <div class="media-left">
-              <a href="#">
-                <img src="http://placehold.it/50x50" alt="..." class="img-circle">
+      <div class="col-md-5">
+        <div class="media">
+          <div class="media-left">      
+            <?php if( get_field('reasons_link_two') ): ?>
+              <a href="<?php the_field('reasons_link_two'); ?>">
+                 <?php if( get_field('reasons_image_two') ): ?>
+                    <img src="<?php the_field('reasons_image_two'); ?>" class="img-circle" width="50" height="50">
+                  <?php endif; ?>
               </a>
-            </div>
-            <div class="media-body">
-              <h4 class="media-heading">Media heading</h4>
-              ...
-            </div>
+            <?php endif; ?>
           </div>
+          
+          <?php if( get_field('reasons_heading_two') ): ?>
+              <div class="media-body">
+                <h4 class="media-heading"><?php the_field('reasons_heading_two'); ?></h4>
+                <?php if( get_field('reasons_body_two') ): ?>
+                  <?php if( get_field('reasons_body_two') ): ?>
+                <?php endif; ?>
+              </div>        
+          <?php endif; ?>
         </div>
       </div>
-       
-       <div class="row">
-        <div class="col-md-5 col-md-offset-2">
-          <div class="media">
-            <div class="media-left">
-              <a href="#">
-                <img src="http://placehold.it/50x50" alt="..." class="img-circle">
+    </div>
+    
+    <div class="row">
+      <div class="col-md-5 col-md-offset-2">
+        <div class="media">
+          <div class="media-left">      
+            <?php if( get_field('reasons_link_three') ): ?>
+              <a href="<?php the_field('reasons_link_three'); ?>">
+                 <?php if( get_field('reasons_image_three') ): ?>
+                    <img src="<?php the_field('reasons_image_three'); ?>" class="img-circle" width="50" height="50">
+                  <?php endif; ?>
               </a>
-            </div>
-            <div class="media-body">
-              <h4 class="media-heading">Media heading</h4>
-              ...
-            </div>
+            <?php endif; ?>
           </div>
-        </div>
-        
-        <div class="col-md-5">
-          <div class="media">
-            <div class="media-left">
-              <a href="#">
-                <img src="http://placehold.it/50x50" alt="..." class="img-circle">
-              </a>
-            </div>
-            <div class="media-body">
-              <h4 class="media-heading">Media heading</h4>
-              ...
-            </div>
-          </div>
+          
+          <?php if( get_field('reasons_heading_three') ): ?>
+              <div class="media-body">
+                <h4 class="media-heading"><?php the_field('reasons_heading_three'); ?></h4>
+                <?php if( get_field('reasons_body_three') ): ?>
+                  <?php if( get_field('reasons_body_three') ): ?>
+                <?php endif; ?>
+              </div>        
+          <?php endif; ?>
         </div>
       </div>
-       
-       <div class="row">
-        <div class="col-md-5 col-md-offset-2">
-          <div class="media">
-            <div class="media-left">
-              <a href="#">
-                <img src="http://placehold.it/50x50" alt="..." class="img-circle">
-              </a>
-            </div>
-            <div class="media-body">
-              <h4 class="media-heading">Media heading</h4>
-              ...
-            </div>
-          </div>
-        </div>
         
-        <div class="col-md-5">
-          <div class="media">
-            <div class="media-left">
-              <a href="#">
-                <img src="http://placehold.it/50x50" alt="..." class="img-circle">
+      <div class="col-md-5">
+        <div class="media">
+          <div class="media-left">      
+            <?php if( get_field('reasons_link_four') ): ?>
+              <a href="<?php the_field('reasons_link_four'); ?>">
+                 <?php if( get_field('reasons_image_four') ): ?>
+                    <img src="<?php the_field('reasons_image_four'); ?>" class="img-circle" width="50" height="50">
+                  <?php endif; ?>
               </a>
-            </div>
-            <div class="media-body">
-              <h4 class="media-heading">Media heading</h4>
-              ...
-            </div>
+            <?php endif; ?>
           </div>
+          
+          <?php if( get_field('reasons_heading_four') ): ?>
+              <div class="media-body">
+                <h4 class="media-heading"><?php the_field('reasons_heading_four'); ?></h4>
+                <?php if( get_field('reasons_body_four') ): ?>
+                  <?php if( get_field('reasons_body_four') ): ?>
+                <?php endif; ?>
+              </div>        
+          <?php endif; ?>
         </div>
       </div>
-     </section>
-     
-     <section>
+    </div>
+    
+    <div class="row">
+      <div class="col-md-5 col-md-offset-2">
+        <div class="media">
+          <div class="media-left">      
+            <?php if( get_field('reasons_link_five') ): ?>
+              <a href="<?php the_field('reasons_link_five'); ?>">
+                 <?php if( get_field('reasons_image_five') ): ?>
+                    <img src="<?php the_field('reasons_image_five'); ?>" class="img-circle" width="50" height="50">
+                  <?php endif; ?>
+              </a>
+            <?php endif; ?>
+          </div>
+          
+          <?php if( get_field('reasons_heading_five') ): ?>
+              <div class="media-body">
+                <h4 class="media-heading"><?php the_field('reasons_heading_five'); ?></h4>
+                <?php if( get_field('reasons_body_five') ): ?>
+                  <?php if( get_field('reasons_body_five') ): ?>
+                <?php endif; ?>
+              </div>        
+          <?php endif; ?>
+        </div>
+      </div>
+        
+      <div class="col-md-5">
+        <div class="media">
+          <div class="media-left">      
+            <?php if( get_field('reasons_link_six') ): ?>
+              <a href="<?php the_field('reasons_link_six'); ?>">
+                 <?php if( get_field('reasons_image_six') ): ?>
+                    <img src="<?php the_field('reasons_image_six'); ?>" class="img-circle" width="50" height="50">
+                  <?php endif; ?>
+              </a>
+            <?php endif; ?>
+          </div>
+          
+          <?php if( get_field('reasons_heading_six') ): ?>
+              <div class="media-body">
+                <h4 class="media-heading"><?php the_field('reasons_heading_six'); ?></h4>
+                <?php if( get_field('reasons_body_six') ): ?>
+                  <?php if( get_field('reasons_body_six') ): ?>
+                <?php endif; ?>
+              </div>        
+          <?php endif; ?>
+        </div>
+      </div>
+    </div>
+    
+     <div class="row">
+      <div class="col-md-5 col-md-offset-2">
+        <div class="media">
+          <div class="media-left">      
+            <?php if( get_field('reasons_link_seven') ): ?>
+              <a href="<?php the_field('reasons_link_seven'); ?>">
+                 <?php if( get_field('reasons_image_seven') ): ?>
+                    <img src="<?php the_field('reasons_image_seven'); ?>" class="img-circle" width="50" height="50">
+                  <?php endif; ?>
+              </a>
+            <?php endif; ?>
+          </div>
+          
+          <?php if( get_field('reasons_heading_seven') ): ?>
+              <div class="media-body">
+                <h4 class="media-heading"><?php the_field('reasons_heading_seven'); ?></h4>
+                <?php if( get_field('reasons_body_seven') ): ?>
+                  <?php if( get_field('reasons_body_seven') ): ?>
+                <?php endif; ?>
+              </div>        
+          <?php endif; ?>
+        </div>
+      </div>
+        
+      <div class="col-md-5">
+        <div class="media">
+          <div class="media-left">      
+            <?php if( get_field('reasons_link_eight') ): ?>
+              <a href="<?php the_field('reasons_link_eight'); ?>">
+                 <?php if( get_field('reasons_image_eight') ): ?>
+                    <img src="<?php the_field('reasons_image_eight'); ?>" class="img-circle" width="50" height="50">
+                  <?php endif; ?>
+              </a>
+            <?php endif; ?>
+          </div>
+          
+          <?php if( get_field('reasons_heading_eight') ): ?>
+              <div class="media-body">
+                <h4 class="media-heading"><?php the_field('reasons_heading_eight'); ?></h4>
+                <?php if( get_field('reasons_body_eight') ): ?>
+                  <?php if( get_field('reasons_body_eight') ): ?>
+                <?php endif; ?>
+              </div>        
+          <?php endif; ?>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  
+  <section>
           <div class="row">
             <div class="col-md-4">
               <div class="front-page-feature-block">
